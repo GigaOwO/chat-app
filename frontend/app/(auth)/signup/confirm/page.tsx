ca935f44257951fa1ebfb896812f678b2247ad51
GigaOwO <takumagiga1118@gmail.com>
2024-12-28 05:46:00 +0900

    page

---
+++ frontend/app/(auth)/signup/confirm/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpConfirmContainer } from './_containers/SignUpContainers/container'
+
+export default function SignUpConfirmPage() {
+  return (
+    <div className="container mx-auto py-8">
+      <SignUpConfirmContainer />
+    </div>
+  )
+}
