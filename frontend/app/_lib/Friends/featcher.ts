3276def8a7f7e55129b8d4e4a16f5f9a71d6b7f1
kmjak <tymkoron1221@icloud.com>
2025-01-16 09:44:34 +0900

    Friendのfeatcherを作った

--- frontend/app/_lib/Friends/featcher.ts
+++ frontend/app/_lib/Friends/featcher.ts
@@ -1,10 +1,25 @@
 import { gql } from 'graphql-request';
 
-export const getFriend = gql`
-`;
-
 export const getFriends = gql`
+  query getFriends {
+    getFriends(friendId: "", userId: "") {
+      createdAt
+      friendId
+      status
+      updatedAt
+      userId
+    }
+  }
 `;
 
 export const deleteFriend = gql`
+  mutation deleteFriend {
+    deleteFriends(input: {userId: "", friendId: ""}) {
+      createdAt
+      friendId
+      status
+      updatedAt
+      userId
+    }
+  }
 `;
